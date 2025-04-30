94cc1b0302edf51f12cf00e490101446294a4247
waik1ki <arbutus@outlook.kr>
2025-04-30 16:02:05 +0900

    chore: directory 변경

--- gateway/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AnalyzerModule } from './analyzer/analyzer.module';
-
-@Module({
-  imports: [AnalyzerModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
